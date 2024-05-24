fd704baf77cd1807757ad1a58e242c6cc96e9e18
rrrreagan <rince@uw.edu>
2024-05-24 12:02:59 -0700

    finished a4

--- Chart3.R
+++ Chart3.R
@@ -1,15 +1,24 @@
-
-library(tidyverse) # for ggplot
-library(dplyr)
-library(ggplot2)
-library(sf)
-library(gridExtra)
-library(grid)
+library(tidyverse)
+library(usmap)
 
 prison_jail_rate_since_1990 <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-jail-rates-1990.csv?raw=true")
 
+prison_cleaned <- prison_jail_rate_since_1990 %>%
+  filter(!is.na(black_prison_pop_rate)) %>%
+  filter(year == 2016) %>%
+  select(state, year, black_prison_pop_rate)
 
-jail_latinx <- prison_jail_rate_since_1990 %>%
-  select(year, state, total_jail_pop_rate, latinx_jail_pop_rate)
+merged_data <- prison_cleaned %>%
+  group_by(state) %>%
+  summarize(avg_black_jail_pop = mean(black_prison_pop_rate, na.rm = TRUE)) %>%
+  na.omit() 
 
-us_sf <- read_sf("tl_2023_us_state.shp")
+usmap_data <- merged_data %>%
+  mutate(state = tolower(state)) 
+
+plot_usmap(data = usmap_data, values = "avg_black_jail_pop", color = "white") +
+  scale_fill_continuous(name = "Average Black Jail Pop Rate", 
+                        label = scales::comma, 
+                        low = "lightsteelblue1", 
+                        high = "dodgerblue3") +
+  labs(title = "Average Black Jail Population Rate by State (2016)")
